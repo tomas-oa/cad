37dbc3edee0e2153c320060977a85fdc0564759e
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-05-21 20:44:34 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App () {
+  const initialState = {
+    from: 'plain',
+    to: 'enctypted',
+    original: '',
+    result: '',
+    loading: false
+  }
+
+  return (
+    <main>
+      <h1>wenas</h1>
+    </main>
+  )
+}
