6374deae2c7805764a723e756a873f51d9cbf91f
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-05-25 22:43:25 -0400

    copyright typo, fixed favicon

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ export default function App () {
         <h5>
           Made with <span className='text-red-500'>♥</span> by <a href="https://github.com/ivnisc" target='_blank' rel='noreferrer' className='text-blue-500'>ivnisc</a> & <a href='https://github.com/tomas-oa' target='_blank' rel='noreferrer' className='text-blue-500'>tomas-oa</a>
         </h5>
-        <small>© Copyright 2023 by <a className='text-white' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank' rel='noreferrer'>Jipi Corp S.A.</a></small>
+        <small>© Copyright 2023 <a className='text-white' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank' rel='noreferrer'>Jipi Corp S.A.</a></small>
       </footer>
 
       <Toaster
